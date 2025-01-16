e78b88e9b2b76e0be1dca2a3c743e57aca0a452a
DaniOspina <danielaos94@gmail.com>
2025-01-16 17:54:52 -0500

    dashboard

---
+++ Página de inicio/pagina_de_inicio.js
@@ -0,0 +1,12 @@
+// Agregar animaciones con scroll
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 100) {
+            section.classList.add("animate");
+        }
+    });
+});
+
+
