c867c4176659b32c2cbd7ed2a5b45eb7529a1034
Ignacio Castaneda97 <castanedaignacio25@gmail.com>
2025-02-25 16:48:17 -0500

    cambios login

--- Login/login.js
+++ Login/login.js
@@ -1,72 +1,95 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('loginForm');
-    const togglePassword = document.querySelector('.toggle-password');
-    const passwordInput = document.getElementById('password');
+const API_URL = "http://localhost:8080/auth";
+    const loginForm = document.getElementById("loginForm");
+const token = localStorage.getItem("token");
+    if (loginForm) {
+        loginForm.addEventListener("submit", async function (event) {
+            event.preventDefault();
+            const email = document.getElementById("email").value;
+            const password = document.getElementById("password").value;
+            if (!email || !password) { 
+                console.error("Error: Email o password no pueden ser null o undefined");
+                 return; }
+            try {
+                
+                console.log("dentro de try "+email+password);
+
+          
+                const response = await fetch(`${API_URL}/login`, {
+                    method: "POST",
+
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": "application/json"
+                    },
+
+                    body: JSON.stringify({email: document.getElementById("email").value, password: document.getElementById("password").value})
+                         
+
+                });
+
+                console.log(JSON.stringify({ email, password })); // Verificar que se envía el JSON correcto
+        
+                
+                if (response.ok) {
+                    console.log("dentro de if");
+
+                    const token = await response.text(); // Obtener el token del JSON
+
+                    localStorage.setItem("jwt", token); // Guardar el token en localStorage
+
+                    window.location.href = "../Productos/productos.html"; // Redirigir a la página protegida
+                } else {
+                    const errorMessage = await response.text();
+
+                    document.getElementById(
+                        "error-message"
+                    ).textContent = `Error en el login: ${errorMessage}`;
+                }
+            } catch (error) {
+                console.error("Error during login:", error);
+            }
+        });
+    }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("loginForm");
+    const togglePassword = document.querySelector(".toggle-password");
+    const passwordInput = document.getElementById("password");
 
     // Validar Correo electrónico (utiliza el type="email" del input)
-    form.email.addEventListener('input', function() {
+    form.email.addEventListener("input", function () {
         const email = form.email.value;
         if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-            document.getElementById('emailError').textContent = 'Correo electrónico inválido.';
+            document.getElementById("emailError").textContent =
+                "Correo electrónico inválido.";
         } else {
-            document.getElementById('emailError').textContent = '';
+            document.getElementById("emailError").textContent = "";
         }
     });
 
     // Validar Contraseña (debe tener al menos 8 caracteres)
-    form.password.addEventListener('input', function() {
+    form.password.addEventListener("input", function () {
         const password = form.password.value;
         if (password.length < 8) {
-            document.getElementById('passwordError').textContent = 'La contraseña debe tener al menos 8 caracteres.';
+            document.getElementById("passwordError").textContent =
+                "La contraseña debe tener al menos 8 caracteres.";
         } else {
-            document.getElementById('passwordError').textContent = '';
+            document.getElementById("passwordError").textContent = "";
         }
     });
 
     // Alternar visibilidad de la contraseña
-    togglePassword.addEventListener('click', function() {
-        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-        passwordInput.setAttribute('type', type);
-        // Cambiar el icono 
-        this.classList.toggle('bi-eye');
-                this.classList.toggle('bi-eye-slash');
+    togglePassword.addEventListener("click", function () {
+        const type =
+            passwordInput.getAttribute("type") === "password" ? "text" : "password";
+        passwordInput.setAttribute("type", type);
+        // Cambiar el icono
+        this.classList.toggle("bi-eye");
+        this.classList.toggle("bi-eye-slash");
     });
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-     
-        // Obtener los valores de los campos del formulario
-        const email = form.email.value;
-        const password = form.password.value;
-     
-        // Limpiar mensajes de error previos
-        document.getElementById('emailError').textContent = '';
-        document.getElementById('passwordError').textContent = '';
-     
-        // Enviar solicitud de inicio de sesión al servidor
-        fetch('http://localhost:8080/auth/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email: email, password: password })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                window.location.href = "../Productos/productos.html";
-            } else {
-                if (data.error === 'email') {
-                    document.getElementById('emailError').textContent = 'Correo electrónico no registrado.';
-                } else if (data.error === 'password') {
-                    document.getElementById('passwordError').textContent = 'Contraseña incorrecta. Inténtalo nuevamente.';
-                }
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            // Manejar error de red o del servidor
-        });
-    });
+    
 });
+
+
