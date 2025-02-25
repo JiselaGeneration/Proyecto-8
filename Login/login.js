80721682897df2e3d57abb37b5fc1cbe76781c82
Ignacio Castaneda97 <castanedaignacio25@gmail.com>
2025-02-25 12:02:02 -0500

    inicio de sesion

--- Login/login.js
+++ Login/login.js
@@ -35,30 +35,38 @@ document.addEventListener('DOMContentLoaded', function() {
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-
+     
         // Obtener los valores de los campos del formulario
         const email = form.email.value;
         const password = form.password.value;
-
+     
         // Limpiar mensajes de error previos
         document.getElementById('emailError').textContent = '';
         document.getElementById('passwordError').textContent = '';
-
-        // Obtener los registros de usuarios del Local Storage
-        const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-
-        // Buscar el usuario por email
-        const usuario = usuarios.find(user => user.email === email);
-
-        if (usuario) {
-            // Verificar la contraseña
-            if (usuario.password === password) {
+     
+        // Enviar solicitud de inicio de sesión al servidor
+        fetch('http://localhost:8080/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email: email, password: password })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
                 window.location.href = "../Productos/productos.html";
             } else {
-                document.getElementById('passwordError').textContent = 'Contraseña incorrecta. Inténtalo nuevamente.';
+                if (data.error === 'email') {
+                    document.getElementById('emailError').textContent = 'Correo electrónico no registrado.';
+                } else if (data.error === 'password') {
+                    document.getElementById('passwordError').textContent = 'Contraseña incorrecta. Inténtalo nuevamente.';
+                }
             }
-        } else {
-            document.getElementById('emailError').textContent = 'Correo electrónico no registrado.';
-        }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            // Manejar error de red o del servidor
+        });
     });
 });
